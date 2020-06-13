0327ecfdf51b971433446f2425940d693eddca78
vishal <vishal.patel929@gmail.com>
2020-06-13 17:37:29 -0400

    Added more logic. Going into algorithm steps now. Will work on backtracking soon.

--- sudokuSolver.cpp
+++ sudokuSolver.cpp
@@ -20,18 +20,15 @@ class Sudoku{
 		int num;
 		//numbers that are prohibited for this entry to take
 		//based on sudoku rules
-		unordered_set <int> prohibitedNums;
-		//length of possibleNums saved
-		int numPossible;
+		unordered_set <int> availableNums;
 
 		SudokuEntry(){
-			this->given=false;
-			this->num=-1;
+			given=false;
+			num=-1;
 			availableNums=new unordered_set <int>;	
 			for (int i=1;i<10;i++){
 				availableNums.insert(i);
 			}
-			this->numPossible=9;
 		}
 
 	}
@@ -48,7 +45,9 @@ class Sudoku{
 	vector <int> givens;
 
 	//sudoku board has a stack of operations to help backtrack when we hit a dead end
-	stack<> ops;
+	//the stack stores the position of the last modified SudokuEntry
+	//WE NEED SOME WAY TO FIGURE OUT EXACTLY WHICH NUMS TO TRY
+	stack<int> ops;
 
 	Sudoku(size){
 		//creating our sudoku object
@@ -57,7 +56,8 @@ class Sudoku{
 		this->sizeSquared=pow(size,2)
 
 		//initializing our SudokuEntries
-		entries = new SudokuEntry[size];	
+		//we have sizeSquared x sizeSquared entries
+		entries = new SudokuEntry[sizeSquared*sizeSquared];	
 
 		givens = new vector <int>;
 		
@@ -90,8 +90,25 @@ class Sudoku{
 
 	//method that sets a whole block as a forbidden value
 	//column from 0-sizeSquared-1 and row from 0-sizeSquared-1
-	void setBlock(int forbidden,int row, int column){
+	void setBlock(int forbidden,int row, int col){
 		//block is nxn or size x size
+		//row should be a multiple of size and same with column
+		//if not, then I should pick the last multiple of size for both and iterate through that block
+		rowOffset = row%size;
+		colOffset=col%size;
+		fixedRow=row-rowOffset;
+		fixedCol=col-colOffset;
+		for (int i=fixedRow;i<fixedRow+size;i++){
+			for (int j=fixedCol;j<fixedCol+size;j++){
+
+				//setting forbidden values for each entry
+				SudokuEntry toConsider = entries[i+(j*sizeSquared)];
+				if (toConsider->given==false){
+					toConsider->availableNums.erase(forbidden);
+				}
+			}
+
+		}
 	}
 	// method to set entry details based on the entries in the same column and row and block
 	void setInitialEntries(){
@@ -99,7 +116,34 @@ class Sudoku{
 		//once I set a given, I should iterate along the row and column of the given to "remove" that given value from the entries possible values
 		//I should also iterate in the block of the given and "remove" the given value from the entries possible values
 		//I will just use the helper methods created above
+		
+		//first index has row, second index has column, third index has the value
+		for (int i=0;i<givens.size();i+=3){
+			row=givens[i];	
+			col=givens[i+1];
+			value=givens[i+2];
+
+			//setting the entry
+			SudokuEntry toConsider = entries[col+(row*sizeSquared)];
+			toConsider->given=true;
+			toConsider->num=value;
+			
+			//setting forbidden entries in the same block, column, and row as this entry
+			setRow(value,row);
+			setColumn(value,col);
+			setBlock(value,row,col);
+
+
+		}
+		//now all the given entries should be set
 	}
+
+	//finds the sudoku entry with the least number of available choices
+	int findEntryToFill(){
+
+	}
+
+	
 };
 
 int main(){
